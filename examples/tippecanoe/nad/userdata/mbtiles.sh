6e1bc893ba0e40806aab7424550268ea7c0e09eb
Ian Shiland <ishiland@gmail.com>
2022-01-21 16:04:33 -0500

    case sensitive S3 path, also need more RAM in ec2

--- examples/tippecanoe/nad/userdata/mbtiles.sh
+++ examples/tippecanoe/nad/userdata/mbtiles.sh
@@ -15,11 +15,11 @@ sudo mkdir /var/data
 sudo chown ec2-user:ec2-user /var/data
 cd /var/data
 # NOTE this file will need to change based on the source that needs to be generated
-aws s3 cp s3://geoplatform-cdn-temp/tippecanoe/nad/nad_r8.geojson.gz nad_r8.json.gz
+aws s3 cp s3://geoplatform-cdn-temp/tippecanoe/NAD/nad_r8.geojson.gz nad_r8.json.gz
 
 # Process data
 tippecanoe -o ./tiles.mbtiles --drop-densest-as-needed --coalesce-densest-as-needed -Z1 -z7 -P -l nad ./nad_r8.json.gz
 
 # Store the created mbtiles file back to S3. This S3 file will need to change as needed for the variations generated
-aws s3 cp tiles.mbtiles s3://geoplatform-cdn-temp/tippecanoe/nad/nad_r8_01.mbtiles
+aws s3 cp tiles.mbtiles s3://geoplatform-cdn-temp/tippecanoe/NAD/nad_r8_01.mbtiles
 sudo shutdown now
