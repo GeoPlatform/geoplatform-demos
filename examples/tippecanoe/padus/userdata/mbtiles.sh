34f002879479eb18fbff35b230d7d43024e3dce7
Ian Shiland <ishiland@gmail.com>
2022-01-31 15:06:08 -0500

    remove combination layers, fix marine layer striping, dont coalesce for styling purposes

--- examples/tippecanoe/padus/userdata/mbtiles.sh
+++ examples/tippecanoe/padus/userdata/mbtiles.sh
@@ -3,7 +3,7 @@
 TILESERVER_URL=s3://gp-sit-tileservice-tile-cache/vector/a7562280_01d9_55bd_aa44_c7cc88f3b137
 MBTILE_URL=s3://gp-sit-us-east-1-geoplatform-archive/a7562280_01d9_55bd_aa44_c7cc88f3b137.mbtiles
 BASE_PATH=s3://geoplatform-cdn-temp/tippecanoe/padus
-TIPPE_PARAMS="-f -P --coalesce-densest-as-needed --extend-zooms-if-still-dropping --generate-ids -zg --maximum-tile-bytes=250000"
+TIPPE_PARAMS="-f -P  --extend-zooms-if-still-dropping --generate-ids -zg --maximum-tile-bytes=250000"
 
 sudo yum update -y
 sudo yum install -y git gcc sqlite-devel.x86_64
@@ -19,30 +19,6 @@ cd /home/ec2-user
 sudo mkdir -p tmp mbtiles tiles
 sudo chown ec2-user:ec2-user ./tmp ./mbtiles ./tiles
 
-
-LAYER=combined_fee_designation
-aws s3 cp ${BASE_PATH}/geojson/${LAYER}.geojsonseq.gz ${PWD}/tmp/${LAYER}.geojsonseq.gz
-tippecanoe ${TIPPE_PARAMS} -l ${LAYER} -o ${PWD}/mbtiles/${LAYER}.mbtiles ${PWD}/tmp/${LAYER}.geojsonseq.gz
-aws s3 cp ${PWD}/mbtiles/${LAYER}.mbtiles ${BASE_PATH}/mbtiles/${LAYER}.mbtile 
-sudo rm ${PWD}/tmp/${LAYER}.geojsonseq.gz
-
-LAYER=combined_fee_designation_easement
-aws s3 cp ${BASE_PATH}/geojson/${LAYER}.geojsonseq.gz ${PWD}/tmp/${LAYER}.geojsonseq.gz
-tippecanoe ${TIPPE_PARAMS} -l ${LAYER} -o ${PWD}/mbtiles/${LAYER}.mbtiles ${PWD}/tmp/${LAYER}.geojsonseq.gz
-aws s3 cp ${PWD}/mbtiles/${LAYER}.mbtiles ${BASE_PATH}/mbtiles/${LAYER}.mbtile 
-sudo rm ${PWD}/tmp/${LAYER}.geojsonseq.gz
-
-LAYER=combined_fee_easement
-aws s3 cp ${BASE_PATH}/geojson/${LAYER}.geojsonseq.gz ${PWD}/tmp/${LAYER}.geojsonseq.gz
-tippecanoe ${TIPPE_PARAMS} -l ${LAYER} -o ${PWD}/mbtiles/${LAYER}.mbtiles ${PWD}/tmp/${LAYER}.geojsonseq.gz
-aws s3 cp ${PWD}/mbtiles/${LAYER}.mbtiles ${BASE_PATH}/mbtiles/${LAYER}.mbtile 
-sudo rm ${PWD}/tmp/${LAYER}.geojsonseq.gz
-
-LAYER=combined_marine_fee_designation_easement
-aws s3 cp ${BASE_PATH}/geojson/${LAYER}.geojsonseq.gz ${PWD}/tmp/${LAYER}.geojsonseq.gz
-tippecanoe ${TIPPE_PARAMS} -l ${LAYER} -o ${PWD}/mbtiles/${LAYER}.mbtiles ${PWD}/tmp/${LAYER}.geojsonseq.gz
-aws s3 cp ${PWD}/mbtiles/${LAYER}.mbtiles ${BASE_PATH}/mbtiles/${LAYER}.mbtile 
-sudo rm ${PWD}/tmp/${LAYER}.geojsonseq.gz
 
 LAYER=designation
 aws s3 cp ${BASE_PATH}/geojson/${LAYER}.geojsonseq.gz ${PWD}/tmp/${LAYER}.geojsonseq.gz
@@ -64,7 +40,7 @@ sudo rm ${PWD}/tmp/${LAYER}.geojsonseq.gz
 
 LAYER=marine
 aws s3 cp ${BASE_PATH}/geojson/${LAYER}.geojsonseq.gz ${PWD}/tmp/${LAYER}.geojsonseq.gz
-tippecanoe ${TIPPE_PARAMS} -l ${LAYER} -o ${PWD}/mbtiles/${LAYER}.mbtiles ${PWD}/tmp/${LAYER}.geojsonseq.gz
+tippecanoe ${TIPPE_PARAMS} --detect-longitude-wraparound -l ${LAYER} -o ${PWD}/mbtiles/${LAYER}.mbtiles ${PWD}/tmp/${LAYER}.geojsonseq.gz
 aws s3 cp ${PWD}/mbtiles/${LAYER}.mbtiles ${BASE_PATH}/mbtiles/${LAYER}.mbtile 
 sudo rm ${PWD}/tmp/${LAYER}.geojsonseq.gz
 
@@ -76,10 +52,6 @@ sudo rm ${PWD}/tmp/${LAYER}.geojsonseq.gz
 
 # join all to a single mbtiles file
 tile-join -pk -f -n PADUS -o mbtiles/PADUS.mbtiles \
-    ${PWD}/mbtiles/combined_fee_designation.mbtiles \
-    ${PWD}/mbtiles/combined_fee_designation_easement.mbtiles \
-    ${PWD}/mbtiles/combined_fee_easement.mbtiles \
-    ${PWD}/mbtiles/combined_marine_fee_designation_easement.mbtiles \
     ${PWD}/mbtiles/designation.mbtiles \
     ${PWD}/mbtiles/easement.mbtiles \
     ${PWD}/mbtiles/fee.mbtiles \
