41e3eae488d0983974c6791483d344c592fcb1b4
Ian Shiland <ishiland@gmail.com>
2022-06-13 13:28:38 -0400

    change plss s3 location

--- examples/plss/deploy.sh
+++ examples/plss/deploy.sh
@@ -1,1 +1,1 @@
-aws s3 cp . s3://geoplatform-cdn-temp/gplayers/ --recursive --exclude deploy.sh --profile sit
+aws s3 cp . s3://geoplatform-cdn-temp/plss/ --recursive --exclude deploy.sh --profile sit
